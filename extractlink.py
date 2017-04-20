8c1ec554c26553fdf39550d26e4b8c35ca85e1af
Abitha Ramachandran <Abitha@dhcp-10-105-166-7.wireless.northwestern.private>
2017-04-19 20:47:37 -0500

    4.19.17

--- extractlink.py
+++ extractlink.py
@@ -5,8 +5,18 @@ import re
 from datetime import datetime
 import xlsxwriter
  
-html_page = urllib2.urlopen("http://variety.com/v/film/")
+html_page = urllib2.urlopen("http://variety.com/v/film/page/5/")
 soup = BeautifulSoup(html_page)
+
+headlineFilter = {"starts": 1, "adds": 1, "moving forward": 2, "in talks": 1, "to board":2, "boards":2, "teams up":1, 
+"teams with":1, "reteams up": 1, "reteams with": 1, "picked up": 1, "picks up": 1,
+"to star in": 2, "nabs": 1, "snags": 1, "in the works": 3, "lands at": 2, "sales": 1, "joins": 1, 
+"to join": 2, "buys": 2, "to direct": 3, "to produce": 3, "casts": 2, "to play": 2, "debut": 1, 
+"to make": 3, "acquire": 2, "development": 3, "circles": 2, "circling": 2, "sequel": -3, "trailer": -3, 
+"dies": -3, "box office": -3, "film review": -3, "clip": -1, "remembers": -1, "release": -1, "award": -2,
+"nominations": -3, "poll": -3, "$": -2, "oscars": -3, "rules": -1, "release": -1, "remember": -2, 
+"premiere": -2, "spinoff": -3, "reboot": -3, "studio": -2, "sales": -1}
+
 
 
 links = []
@@ -20,6 +30,9 @@ for link in soup.findAll('a', attrs={'href': re.compile("^http://variety.com/[a-
 
 
 movieTitle = []
+tagsHeader = []
+
+#print(headlineFilter)
 
 for link in links:
 #link = links[0]
@@ -29,14 +42,44 @@ for link in links:
 	tags = soup1.findAll('h1')
 	
 	tagsStr = str(tags[0])
+	headerValue = 0;
 
-	start = tagsStr.find('&#8216;') + 7
-	end = tagsStr.find('&#8217', start)
+	#print(tagsStr)
+	for key in headlineFilter.keys():
+		if key in tagsStr.lower():
+			#print("it got here")
+			#print(key)
+			#print(tagsStr)
+			headerValue = headerValue + headlineFilter[key]
+	
 
-	if ("</h1" in tagsStr[start:end] or tagsStr[start:end] in movieTitle):
+	if(headerValue > 0):
+		tagsHeader.append(tagsStr.lower())
+		start = tagsStr.find('&#8216;') + 7
+		end = tagsStr.find('&#8217', start)
+		if("-" == tagsStr[end+7] 
+			or "director" == tagsStr[end+8:end+16].lower()
+			or "producer" == tagsStr[end+8:end+16]
+			or "star" == tagsStr[end+8:end+12]):
+			temp = tagsStr[end + 8:]
+			start = temp.find('&#8216;') + 7
+			end = temp.find('&#8217', start)
+			
+			if("</h1" in temp[start:end]):
+				start = 4
+				end = temp.find("</h1", start)
+			if (temp[start:end] in movieTitle):
+				continue
+			movieTitle.append(temp[start:end])
 			continue
 
-	movieTitle.append(tagsStr[start:end])
+		if("</h1" in tagsStr[start:end]):
+			start = 4
+			end = tagsStr.find("</h1", start)
+		if (tagsStr[start:end] in movieTitle):
+			continue
+
+		movieTitle.append(tagsStr[start:end])
 	#print(tags)
 	#print(tagsStr[start:end])
 
@@ -44,6 +87,10 @@ for link in links:
 
 workbook = xlsxwriter.Workbook('Expenses03.xlsx')
 worksheet = workbook.add_worksheet()
+
+print(movieTitle)
+#print(len(tagsHeader))
+#print(tagsHeader)
 
 worksheet.set_column(0, 0, 50)
 
